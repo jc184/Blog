2300873ab2e1e620bdd334b69458aa87d162657c
james chalmers <james.chalmers184@gmail.com>
2021-12-04 14:10:01 +0000

    Entities corrected

--- Blog.Contracts/Data/Entities/Comment.cs
+++ Blog.Contracts/Data/Entities/Comment.cs
@@ -9,7 +9,6 @@ namespace Blog.Contracts.Data.Entities
     public class Comment : BaseEntity
     {
         public string Content { get; set; }
-        public DateTime AddedOn { get; set; }
         public Author Author { get; set; }
     }
 }
--- Blog.Contracts/Data/Entities/Post.cs
+++ Blog.Contracts/Data/Entities/Post.cs
@@ -11,7 +11,6 @@ namespace Blog.Contracts.Data.Entities
         public string Title { get; set; }
         public string Body { get; set; } 
         public string Status { get; set; }
-        public DateTime AddedOn { get; set; }
         public Author Author { get; set; }
         public ICollection<Comment> Comments { get; set;}
 
