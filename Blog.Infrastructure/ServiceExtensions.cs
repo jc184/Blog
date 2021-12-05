9cf75a082d1b3a8972aad23f73a6d1afbd8d2ea4
james chalmers <james.chalmers184@gmail.com>
2021-12-05 12:40:57 +0000

    Project Refs resolved, libs added, Program and Startup amended

---
+++ Blog.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Contracts.Data;
+using Blog.Infrastructure.Data;
+using Blog.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        private static IServiceCollection AddUnitOfWork(this IServiceCollection services)
+        {
+            return services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        private static IServiceCollection AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddSqlite<BlogDbContext>(configuration.GetConnectionString("DefaultConnection"), (options) =>
+            {
+                options.MigrationsAssembly("Blog.Migrations");
+            });
+        }
+
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddDatabaseContext(configuration).AddUnitOfWork();
+        }
+    }
+}
+
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,25 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
+using Blog.API;
+using Blog.Migrations;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Blog
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = CreateHostBuilder(args).Build();
+
+            using var scope = builder.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<BlogDbContext>();
+            if (db.Database.GetPendingMigrations().Any())
+            {
+                db.Database.Migrate();
+            }
+
+            builder.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
