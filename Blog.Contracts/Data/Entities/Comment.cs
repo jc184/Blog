b6d8dcce2dcb2322db7a7d7b618b1a85140a29b4
james chalmers <james.chalmers184@gmail.com>
2021-12-20 07:02:14 +0000

    properties made nullable

--- Blog.Contracts/Data/Entities/Comment.cs
+++ Blog.Contracts/Data/Entities/Comment.cs
@@ -8,10 +8,10 @@ namespace Blog.Contracts.Data.Entities
 {
     public class Comment : BaseEntity
     {
-        public string Content { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        public string? Content { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
         public int PostId { get; set; }
     }
 }
--- Blog.Contracts/Data/Entities/Post.cs
+++ Blog.Contracts/Data/Entities/Post.cs
@@ -8,13 +8,13 @@ namespace Blog.Contracts.Data.Entities
 {
     public class Post : BaseEntity
     {
-        public string Title { get; set; }
-        public string Body { get; set; } 
-        public string Status { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public ICollection<Comment> Comments { get; set;}
+        public string? Title { get; set; }
+        public string? Body { get; set; } 
+        public string? Status { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public ICollection<Comment>? Comments { get; set;}
 
     }
 }
